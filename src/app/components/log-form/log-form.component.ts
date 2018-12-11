cc649e178dc24e2ef2fb5ba79fa1d2030bcb27fe
Robert W. Leonard <robert@draknor.com>
2018-12-10 22:55:29 -0600

    CRUD operations on form

--- src/app/components/log-form/log-form.component.ts
+++ src/app/components/log-form/log-form.component.ts
@@ -12,16 +12,50 @@ export class LogFormComponent implements OnInit {
   text: string;
   date: any;
 
+  isNew: boolean = true;
+
   constructor(private LogService: LogService) { }
 
   ngOnInit() {
     // Subscribe to the selectedlog observable
     this.LogService.selectedLog.subscribe(log => {
       if (log.id !== null) {
+        this.isNew = false;
         this.id = log.id;
         this.text = log.text;
         this.date = log.date;
       }
+    });
+  }
+
+  onSubmit() {
+    // Check if new log
+    if (this.isNew) {
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      }
+
+      // Add log
+      this.LogService.addLog(newLog);
+    } else {
+      // Create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      }
+
+      // Update log
+      this.LogService.updateLog(updLog);
+    }
+  }
+
+  generateId() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
     });
   }
 
